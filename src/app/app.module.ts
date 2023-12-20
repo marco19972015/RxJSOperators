0e39bd2d3b7ee284d86c0946f9d878e50b58d2fd
Marco Torres <marcoantonio.torres@icloud.com>
2023-12-20 15:08:51 -0500

    Used the tap operator and showed two examples that log out the value a value coming into the pipe. Tap allows us to take logic out of the subscribe method and can be used to save a value into a variable such as an array.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MapOperatorComponent } from './map-operator/map-operator.component';
+import { TapOperatorComponent } from './tap-operator/tap-operator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapOperatorComponent
+    MapOperatorComponent,
+    TapOperatorComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/tap-operator/tap-operator.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, from, map, of, tap } from 'rxjs';
+
+@Component({
+  selector: 'app-tap-operator',
+  templateUrl: './tap-operator.component.html',
+  styleUrls: ['./tap-operator.component.css']
+})
+export class TapOperatorComponent implements OnInit, OnDestroy{
+  // Applying the concept of tap
+
+  subApples!: Subscription;
+  subNumbers!: Subscription;
+
+  ngOnInit(){
+    const apples$ = from([ 
+      { id: 1, type: 'macintosh'},
+      { id: 2, type: 'gala'},
+      { id: 3, type: 'fugi'}
+    ]);
+
+    // Added the tap operator to display initial value and then manipulated value 
+    this.subNumbers = of(10, 30, 50).pipe(
+      tap(value => console.log("Initial value is: ", value)),
+      map(value => value * 2),
+      tap(value => console.log("Value in subNumbers is: ", value))
+    ).subscribe()
+
+    this.subApples = apples$
+      .pipe(
+        map(a => ({ ...apples$, color: 'red'})),
+        // Using the cursor to hover over shows the type of the item 
+        tap(a => console.log('Apple: ', a))
+      ).subscribe(); 
+  }
+
+  ngOnDestroy(){
+    // Remember to unsubscribe
+    this.subApples.unsubscribe();
+    this.subNumbers.unsubscribe();
+  }
+}
